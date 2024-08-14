7e97c738a8034f624477e5c73256ee7ddf435271
Didem Cakir <didemydn@gmail.com>
2024-08-14 15:05:20 +0200

    home and list links implemented

--- src/views/Layout.jsx
+++ src/views/Layout.jsx
@@ -1,4 +1,4 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, NavLink } from 'react-router-dom';
 
 import './Layout.css';
 import { auth } from '../api/config.js';
@@ -33,13 +33,13 @@ export function Layout() {
 				</main>
 				<nav className="Nav">
 					<div className="Nav-container">
-						<a href="#" className="Nav-link">
+						<NavLink to="/home" className="Nav-link">
 							Home
-						</a>
-						<a href="#" className="Nav-link">
+						</NavLink>
+						<NavLink to="/list" className="Nav-link">
 							List
-						</a>
-						<a href="#" className="Nav-link">
+						</NavLink>
+						<a href="/" className="Nav-link">
 							Manage List
 						</a>
 					</div>
