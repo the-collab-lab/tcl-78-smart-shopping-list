0729f4cc781aa249d57fed5f2e8e8d4d59254db8
tataw-cl <127490325+tataw-cl@users.noreply.github.com>
2024-08-21 20:21:50 +0100

    add form and minimal styling to Home.jsx

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -6,19 +6,26 @@ import { createList } from '../api/firebase';
 
 export function Home({ data, setListPath, userId, userEmail }) {
 	const [listName, setListName] = useState('');
+	const [message, setMessage] = useState('');
 	const navigate = useNavigate(); //to call redirected to the List view
 
 	const handleCreateListButton = async (e) => {
 		e.preventDefault();
+		if (!listName) {
+			setMessage('Enter a list name');
+			return;
+		}
 
 		try {
 			await createList(userId, userEmail, listName);
+			setMessage('New list successfully created');
 
 			const createListPath = `${userId}/${listName}}`;
 			setListPath(createListPath);
 			navigate('/list'); //redirect to the list view
 		} catch (error) {
 			console.error('error creating a list', error);
+			setMessage('Failed to create list. Please try again');
 		}
 	};
 
@@ -37,7 +44,22 @@ export function Home({ data, setListPath, userId, userEmail }) {
 					/>
 				))}
 			</ul>
-			<form onSubmit={handleCreateListButton}></form>
+
+			<form onSubmit={handleCreateListButton}>
+				<label htmlFor="listName">List Name:</label>
+				<input
+					type="text"
+					id="listName"
+					value={listName}
+					onChange={(e) => setListName(e.target.value)}
+					placeholder="Enter the name of your new list"
+				/>
+				<button type="submit" className="button">
+					{' '}
+					Create list{' '}
+				</button>
+				{message}
+			</form>
 		</div>
 	);
 }
