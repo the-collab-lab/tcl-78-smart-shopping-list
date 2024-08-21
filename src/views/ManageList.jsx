bcdc0209c62d30e937694c7b70ff105a08f5fe92
Eva Langerova <langerova_eva@yahoo.com>
2024-08-21 18:35:28 +0200

    add form to ManageList view

--- src/views/ManageList.jsx
+++ src/views/ManageList.jsx
@@ -1,7 +1,46 @@
+import { useState } from 'react';
+
 export function ManageList() {
+	const [formData, setFormData] = useState({
+		name: '',
+		nextPurchase: '',
+	});
+
+	const handleChange = (e) => {
+		const { name } = e.target;
+		setFormData((prevFormData) => {
+			return {
+				...prevFormData,
+				[name]: type === 'checkbox' ? checked : value,
+			};
+		});
+	};
+
+	// const handleChange = (e) => {
+	//   const { name, value, type, checked } = e.target;
+	//   setFormData((prevFormData) => {
+	//     return {
+	//       ...prevFormData,
+	//       [name]: type === "checkbox" ? checked : value,
+	//     };
+	//   });
+	// };
+
 	return (
-		<p>
-			Hello from the <code>/manage-list</code> page!
-		</p>
+		<>
+			<form>
+				<label htmlFor="name">Item name</label>
+				<input
+					type="text"
+					id="name"
+					placeholder="Item"
+					// value={formData.name}
+					onChange={handleChange}
+					name="name"
+					required
+				/>
+				<button type="submit">Add Item</button>
+			</form>
+		</>
 	);
 }
