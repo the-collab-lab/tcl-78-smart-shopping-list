87881fbf57e5a3c99d831e4311b466829291cd22
Eva Langerova <langerova_eva@yahoo.com>
2024-08-21 22:09:20 +0200

    update form function in ManageList view to console log new item name and next purchase number

--- src/views/ManageList.jsx
+++ src/views/ManageList.jsx
@@ -3,19 +3,26 @@ import { useState } from 'react';
 export function ManageList() {
 	const [formData, setFormData] = useState({
 		name: '',
-		nextPurchase: '',
+		nextPurchase: 0,
 	});
 
 	const handleChange = (e) => {
-		const { name } = e.target;
+		const { name, value } = e.target;
 		setFormData((prevFormData) => {
 			return {
 				...prevFormData,
-				[name]: type === 'checkbox' ? checked : value,
+				[name]: value,
 			};
 		});
 	};
 
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log(formData);
+		// submit to DB?? (call addItem from firebase.js and pass in the pathList and formData)
+	};
+
+	// template react form function for reference
 	// const handleChange = (e) => {
 	//   const { name, value, type, checked } = e.target;
 	//   setFormData((prevFormData) => {
@@ -28,18 +35,32 @@ export function ManageList() {
 
 	return (
 		<>
-			<form>
+			<form onSubmit={handleSubmit}>
 				<label htmlFor="name">Item name</label>
 				<input
-					type="text"
 					id="name"
+					type="text"
 					placeholder="Item"
-					// value={formData.name}
+					value={formData.name}
 					onChange={handleChange}
 					name="name"
 					required
 				/>
-				<button type="submit">Add Item</button>
+
+				<label htmlFor="nextPurchase">When is your next purchase</label>
+				<select
+					name="nextPurchase"
+					id="nextPurchase"
+					onChange={handleChange}
+					value={formData.nextPurchase}
+				>
+					<option value="">---</option>
+					<option value={7}>Soon</option>
+					<option value={14}>Kind of soon</option>
+					<option value={30}>Not soon</option>
+				</select>
+
+				<button>Add Item</button>
 			</form>
 		</>
 	);
