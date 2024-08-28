7cfa7cfe8c9f985e3f683ac3b8d6576a62f4fe4f
Eva Langerova <langerova_eva@yahoo.com>
2024-08-28 09:05:09 +0200

    add form to ManageList view to share a shopiing list with other users

--- src/views/ManageList.jsx
+++ src/views/ManageList.jsx
@@ -2,28 +2,31 @@ import { useState } from 'react';
 import { addItem } from '../api';
 
 export function ManageList({ listPath }) {
-	const [formData, setFormData] = useState({
+	const [formNewItem, setFormNewItem] = useState({
 		name: '',
 		nextPurchase: 0,
 	});
-	const [message, setMessage] = useState('');
+	const [messageItem, setMessageItem] = useState('');
 
-	const handleChange = (e) => {
+	const [formAddUser, setFormAddUser] = useState('');
+	const [messageUser, setMessageUser] = useState('');
+
+	const handleNewItemChange = (e) => {
 		const { name, value } = e.target;
-		setFormData((prevFormData) => {
+		setFormNewItem((prevForm) => {
 			return {
-				...prevFormData,
+				...prevForm,
 				[name]: value,
 			};
 		});
 	};
 
-	const handleSubmit = async (e) => {
+	const handleNewItemSubmit = async (e) => {
 		e.preventDefault();
-		const { name, nextPurchase } = formData;
+		const { name, nextPurchase } = formNewItem;
 
 		if (!name || !nextPurchase) {
-			setMessage('Please fill out all fields');
+			setMessageItem('Please fill out all fields');
 			return;
 		}
 		try {
@@ -31,27 +34,33 @@ export function ManageList({ listPath }) {
 				itemName: name,
 				daysUntilNextPurchase: nextPurchase,
 			});
-			setMessage(`${name} has been successfully added to the list`);
-			setFormData({
+			setMessageItem(`${name} has been successfully added to the list`);
+			setFormNewItem({
 				name: '',
 				nextPurchase: 0,
 			});
 		} catch (error) {
 			console.log('Failed to add the item: ', error);
-			setMessage('Failed to add the item to the list.');
+			setMessageItem('Failed to add the item to the list.');
 		}
+	};
+
+	const handleAddUserSubmit = async (e) => {
+		e.preventDefault();
+		// to do
 	};
 
 	return (
 		<>
-			<form onSubmit={handleSubmit}>
+			<h2>Add new item to your list</h2>
+			<form onSubmit={handleNewItemSubmit}>
 				<label htmlFor="name">Item name</label>
 				<input
 					id="name"
 					type="text"
 					placeholder="Item"
-					value={formData.name}
-					onChange={handleChange}
+					value={formNewItem.name}
+					onChange={handleNewItemChange}
 					name="name"
 					required
 				/>
@@ -61,8 +70,9 @@ export function ManageList({ listPath }) {
 				<select
 					name="nextPurchase"
 					id="nextPurchase"
-					onChange={handleChange}
-					value={formData.nextPurchase}
+					onChange={handleNewItemChange}
+					value={formNewItem.nextPurchase}
+					required
 				>
 					<option value="">---</option>
 					<option value={7}>Soon</option>
@@ -70,9 +80,27 @@ export function ManageList({ listPath }) {
 					<option value={30}>Not soon</option>
 				</select>
 
-				<p>{message}</p>
+				<p>{messageItem}</p>
 
 				<button>Add Item</button>
+			</form>
+
+			<h2>Invite a user to share your list with you</h2>
+			<form onSubmit={handleAddUserSubmit}>
+				<label htmlFor="email">User email</label>
+				<input
+					id="email"
+					type="email"
+					placeholder="Email"
+					value={formAddUser}
+					onChange={(e) => setFormAddUser(e.target.value)}
+					name="email"
+					required
+				/>
+
+				<p>{messageUser}</p>
+
+				<button>Invite</button>
 			</form>
 		</>
 	);
