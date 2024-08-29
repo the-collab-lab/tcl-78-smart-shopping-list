d247f3c9308209ad794239be64cefbc7e49b48a8
GrannyYetta <you@example.com>
2024-08-29 14:53:37 +0200

    debugged SearchForm and List view

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,36 +1,32 @@
-import { useState } from 'react';
-
-export default function searchForm({ onSearch }) {
-	const [searchItem, setSearchItem] = useState('');
-
+function SearchForm({ onSearch }) {
 	const handleSearch = (e) => {
 		e.preventDefault();
 		const value = e.target.value;
-		setSearchItem(value);
+
 		onSearch(value);
 	};
 
 	const clearSearch = () => {
-		setSearchTerm('');
 		onSearch('');
 	};
 
-	return;
-	<form onSubmit={handleSearch}>
-		<div className="search-input-container">
-			<label htmlFor="search-input">Search items:</label>
-			<input
-				type="text"
-				id="search-input"
-				value={searchItem}
-				placeholder="Item from list"
-				required
-			/>
-			{searchItem && (
+	return (
+		<form onSubmit={handleSearch}>
+			<div className="search-input-container">
+				<label htmlFor="search-input">Search items:</label>
+				<input
+					type="search"
+					id="search-input"
+					placeholder="Item from list"
+					onChange={handleSearch}
+					required
+				/>
 				<button type="button" onClick={clearSearch} className="clear-button">
 					x
 				</button>
-			)}
-		</div>
-	</form>;
+			</div>
+		</form>
+	);
 }
+
+export default SearchForm;
--- src/views/List.jsx
+++ src/views/List.jsx
@@ -1,15 +1,22 @@
 import { ListItem } from '../components';
-import { SearchForm } from '../components/SearchForm';
+import SearchForm from '../components/SearchForm';
+import { useState } from 'react';
 
 export function List({ data }) {
 	const [filteredItems, setFilteredItems] = useState(data);
 
+	const [searchTerm, setSearchTerm] = useState('');
+
+	console.log(data);
+
 	const handleSearch = (searchItem) => {
+		console.log(searchItem);
+		setSearchTerm(searchItem);
 		if (searchItem === '') {
 			setFilteredItems(data);
 		} else {
 			const filtered = data.filter((item) =>
-				item.name.toLowerCase().includes(searchTerm.toLowerCase()),
+				item.name.toLowerCase().includes(searchItem.toLowerCase()),
 			);
 			setFilteredItems(filtered);
 		}
@@ -22,9 +29,11 @@ export function List({ data }) {
 			</p>
 			<SearchForm onSearch={handleSearch} />
 			<ul>
-				{data.map((item) => (
-					<ListItem key={item.id} name={item.name} />
-				))}
+				{searchTerm
+					? filteredItems.map((item) => (
+							<ListItem key={item.id} name={item.name} />
+						))
+					: data.map((item) => <ListItem key={item.id} name={item.name} />)}
 			</ul>
 		</>
 	);
