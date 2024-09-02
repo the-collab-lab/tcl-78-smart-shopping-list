75401a89e507dd0b1281c8fb57ab1571654dec26
Didem Cakir <didemydn@gmail.com>
2024-09-02 14:27:14 +0200

    checkbox implementing

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,5 +1,20 @@
+import { useState } from 'react';
 import './ListItem.css';
 
-export function ListItem({ name }) {
-	return <li className="ListItem">{name}</li>;
+export function ListItem({ name, id, onCheck }) {
+	const { isChecked, setIsChecked } = useState(false);
+
+	const handleChecked = () => {
+		const newCheckedStatus = !isChecked;
+		setIsChecked(newCheckedStatus);
+		onCheck(id, newCheckedStatus);
+	};
+	return (
+		<li className="ListItem">
+			<label>
+				<input type="checkbox" checked={isChecked} onChange={handleChecked} />
+				{name}
+			</label>
+		</li>
+	);
 }
