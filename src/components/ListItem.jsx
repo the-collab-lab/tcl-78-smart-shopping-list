a9d38b03875535eebec5087c0b444e1d1c22e03a
GrannyYetta <you@example.com>
2024-09-16 19:14:30 +0200

    eliminated unnecessary handleCheck

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -4,15 +4,14 @@ import './ListItem.css';
 export function ListItem({ name, dateLastPurchased, onCheck }) {
 	const [isChecked, setIsChecked] = useState(false);
 
-// Update `isChecked` based on the `dateLastPurchased` value
+	// Update `isChecked` based on the `dateLastPurchased` value
 	useEffect(() => {
 		const checkStatus = () => {
 			if (dateLastPurchased) {
 				const purchaseDate = dateLastPurchased.toDate();
 				const timeSinceLastPurchase = new Date() - purchaseDate;
 				const hasBeenPurchasedRecently =
-
-				timeSinceLastPurchase < 24 * 60 * 60 * 1000; // 24 hours
+					timeSinceLastPurchase < 24 * 60 * 60 * 1000; // 24 hours
 				setIsChecked(hasBeenPurchasedRecently);
 			} else {
 				setIsChecked(false);
@@ -21,10 +20,6 @@ export function ListItem({ name, dateLastPurchased, onCheck }) {
 
 		checkStatus();
 	}, [dateLastPurchased]);
-
-	const handleChecked = () => {
-		onCheck(id);
-	};
 
 	return (
 		<li className="ListItem">
