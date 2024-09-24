14d5e0cfa262e66775b4d5ddd538ef31b16215df
Eva Langerova <langerova_eva@yahoo.com>
2024-09-24 19:08:12 +0200

    create a new About view, conditional render of Home view and navbar for anonymous/logged in users

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import { Home, Layout, List, ManageList } from './views';
+import { Home, Layout, List, ManageList, About } from './views';
 
 import { useAuth, useShoppingListData, useShoppingLists } from './api';
 
@@ -55,6 +55,7 @@ export function App() {
 							/>
 						}
 					/>
+					<Route path="/about" element={<About />} />
 					<Route
 						path="/list"
 						element={<List data={data} lists={lists} listPath={listPath} />}
