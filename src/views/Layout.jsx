7bddd57efa9b9b7b506234bf9fdceecc252b06de
Eva Langerova <langerova_eva@yahoo.com>
2024-09-25 18:55:59 +0200

    delete a comment in Layout.jsx

--- src/views/Layout.jsx
+++ src/views/Layout.jsx
@@ -2,14 +2,6 @@ import { Outlet, NavLink, Link } from 'react-router-dom';
 import './Layout.css';
 import { SignInButton, SignOutButton, useAuth } from '../api/useAuth';
 
-/**
- * TODO: The links defined in this file don't work!
- *
- * Instead of anchor element, they should use a component
- * from `react-router-dom` to navigate to the routes
- * defined in `App.jsx`.
- */
-
 export function Layout() {
 	const { user } = useAuth();
 	return (
